006d6e5fa070b58115334cd667a8d2531d587d4a
David Manuel Herrera Rodríguez <davisrey98@live.com>
2017-07-11 21:18:18 +0200

    exe, intelligent so and new colors :D

--- emails.py
+++ emails.py
@@ -1,10 +1,14 @@
 import os
+import os.path
 import smtplib
 import getpass
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-os.system('cls')
+if os.name == 'nt':
+    os.system('cls')
+else:
+    os.system('clear')
 
 print('\x1b[1;31;40m' + """                 
                          (                                      
@@ -16,6 +20,14 @@ print('\x1b[1;31;40m' + """
     |___|_||_| \_/ |_\___/ \__,_\___|  \__\___/_| |_| \___\___/__/  
 \x1b[0m""")
 
+if os.path.isfile('password.txt'):
+    archivo = open('password.txt', 'r')
+
+    if archivo != "":
+        print("\x1b[0;36;40m Cuenta iniciada: " + archivo.readline() +
+              '\x1b[0m')
+    archivo.close()
+
 print("""Menu:
 1) Crear cuenta nueva
 2) Usar cuenta existente
@@ -24,21 +36,20 @@ print("""Menu:
 opcion = int(input(""))
 if opcion == 1:
 
-    me = input('\x1b[0;31;40m' + "Introduce tu correo: \x1b[0m")
-    contra = getpass.getpass("\x1b[0;31;40mIntroduce tu contraseña: \x1b[0m")
+    me = input('\x1b[1;31;40m' + "Introduce tu correo: \x1b[0m")
+    contra = getpass.getpass("\x1b[1;31;40mIntroduce tu contraseña: \x1b[0m")
     archivo = open('password.txt', 'w')
     archivo.write(me + '\n' + contra)
     archivo.close
     print('\x1b[1;32;40m' + "cuenta registrada con exito \x1b[0m")
-
     # datos de ambos
 archivo = open('password.txt', 'r')
 me = archivo.readline()
 contra = archivo.readline()
 
-destino = input('\x1b[0;31;40m' + "A quien quieres enviar el mensaje? \x1b[0m")
-encabezado = input('\x1b[0;31;40m' + "introduce el asunto: \x1b[0m")
-texto = input('\x1b[0;31;40m' + "introduce el texto del mensaje: \x1b[0m")
+destino = input('\x1b[1;31;40m' + "A quien quieres enviar el mensaje? \x1b[0m")
+encabezado = input('\x1b[1;31;40m' + "introduce el asunto: \x1b[0m")
+texto = input('\x1b[1;31;40m' + "introduce el texto del mensaje: \x1b[0m")
 
 msg = MIMEMultipart()
 msg['From'] = me
@@ -54,10 +65,12 @@ elif (me[len(me) - 7] == 'v'):
 elif (me[len(me) - 11] == 'h'):
     server = smtplib.SMTP('smtp.hotmail.com', 25)
 
-print('\x1b[0;33;40m' + "El mensaje va ha ser enviado a: " + destino +
-      '\x1b[0m')
+print('\x1b[0;33;40m' + "El mensaje va ha ser enviado a: \x1b[0m" +
+      "\x1b[1;36;40m" + destino + '\x1b[0m')
 print('\x1b[0;33;40m' + 'El contenido del mensaje es el siguiente: \x1b[0m')
-print("\x1b[1;34;40m" + "   " + texto + '\x1b[0m')
+print('')
+print("\x1b[1;36;40m" + "   " + texto + '\x1b[0m')
+print("")
 decision = input("Quieres enviarlo? ")
 if (decision == 'si'):
     server.starttls()
@@ -69,3 +82,4 @@ if (decision == 'si'):
 else:
     print('\x1b[1;31;40m' + 'Envio cancelado \x1b[0m')
 archivo.close()
+input("presiona enter para salir")
